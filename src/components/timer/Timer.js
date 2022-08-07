795333b4d8ae922d755553571c871600303d9405
Mark <mark.mccormack.2019@mumail.ie>
2022-08-08 00:04:46 +0100

    fix(timerComponent): fixed negative time bug

--- src/components/timer/Timer.js
+++ src/components/timer/Timer.js
@@ -6,26 +6,40 @@ function Timer() {
 
   const [inputSeconds, setInputSeconds] = useState(0);
   const [inputMinutes, setInputMinutes] = useState(0);
-  const [seconds, setSeconds] = useState(1500);
+  const [seconds, setSeconds] = useState(2);
 
   function startTimer() {
     // Decrease Time Every Second by 1 Second
-    timer = setInterval(() => { setSeconds((prevCount) => prevCount - 1) }, 1000);
+    timer = setInterval(() => {
+      setSeconds((seconds) => {
+        seconds -= 1;
 
-    // If Time is Up, Stop the Timer and Open A Modal
-    if (seconds === 0) {
-      clearInterval(timer);
-      openModal();
-    }
+        // If Time is Up, Stop the Timer and Open A Modal
+        if (seconds <= 0) {
+          clearInterval(timer);
+          openModal();
+        }
+
+        return seconds;
+      });
+    }, 1000);
   }
 
-  // Controls to Set Time and Stop the Timer
-  const setTimer = () => {setSeconds(inputSeconds + (inputMinutes * 60))}
-  const stopTimer = () => {clearInterval(timer)}
+  const setTimer = () => {
+    setSeconds(inputSeconds - inputMinutes * 60);
+  };
 
-  // Take User Input Time
-  const updateSeconds = (event) => {setInputSeconds(event.target.value)}
-  const updateMinutes = (event) => {setInputMinutes(event.target.value)}
+  const stopTimer = () => {
+    clearInterval(timer);
+  };
+
+  const updateSeconds = (event) => {
+    setInputSeconds(event.target.value);
+  };
+
+  const updateMinutes = (event) => {
+    setInputMinutes(event.target.value);
+  };
 
   return (
     <div>
@@ -38,15 +52,37 @@ function Timer() {
           </div>
           <div className="controls">
             <div className="field-group flex">
-              <input className="input" type="text" value={inputMinutes} onChange={updateMinutes}/>
+              <input
+                className="input"
+                type="text"
+                value={inputMinutes}
+                onChange={updateMinutes}
+              />
               &nbsp;
-              <input className="input" type="text" value={inputSeconds} onChange={updateSeconds}/>
+              <input
+                className="input"
+                type="text"
+                value={inputSeconds}
+                onChange={updateSeconds}
+              />
             </div>
             <br></br>
             <div className="buttons">
-              <button className="button is-link is-focused" onClick={setTimer}>Set Timer</button>
-              <button className="button is-primary is-focused" onClick={startTimer}>Start Timer</button>
-              <button className="button is-danger is-focused" onClick={stopTimer}>Stop Timer</button>
+              <button className="button is-link is-focused" onClick={setTimer}>
+                Set Timer
+              </button>
+              <button
+                className="button is-primary is-focused"
+                onClick={startTimer}
+              >
+                Start Timer
+              </button>
+              <button
+                className="button is-danger is-focused"
+                onClick={stopTimer}
+              >
+                Stop Timer
+              </button>
             </div>
           </div>
 
@@ -56,20 +92,23 @@ function Timer() {
         </div>
       </div>
 
-      <div id="modal" class="modal">
-        <div class="modal-background"></div>
-        <div class="modal-content">
-
-        <div class="card">
-          <div class="card-content">
-            <div class="content" style={{fontSize: '1.5rem'}}>
-            🍅Your timer has complete. Take 5 minutes to relax!🍅
-              <br></br>
+      <div id="modal" className="modal">
+        <div className="modal-background"></div>
+        <div className="modal-content">
+          <div className="card">
+            <div className="card-content">
+              <div className="content" style={{ fontSize: "1.5rem" }}>
+                🍅Your timer has complete. Take 5 minutes to relax!🍅
+                <br></br>
+              </div>
             </div>
           </div>
         </div>
-        </div>
-        <button class="modal-close is-large" aria-label="close" onClick={closeModal}></button>
+        <button
+          className="modal-close is-large"
+          aria-label="close"
+          onClick={closeModal}
+        ></button>
       </div>
     </div>
   );
@@ -78,11 +117,11 @@ function Timer() {
 const closeModal = () => {
   var element = document.getElementById("modal");
   element.classList.remove("is-active");
-}
+};
 
 const openModal = () => {
   var element = document.getElementById("modal");
   element.classList.add("is-active");
-}
+};
 
 export default Timer;
